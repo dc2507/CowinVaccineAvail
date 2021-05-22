17a913958d097589154296e9850dd9c2c0ba14b4
Devanshu Chaturvedi <67556455+dc2507@users.noreply.github.com>
2021-05-22 19:07:47 +0530

    Added dynamic param for district ID

--- cowinScript.py
+++ cowinScript.py
@@ -2,18 +2,19 @@ from pushbullet import PushBullet
 import http.client
 from datetime import date, datetime
 import json
+import sys
 
 # figuring out date in specified format
 today = date.today()
 d1 = today.strftime("%d-%m-%Y")
-
+dist_id=sys.argv[1]
 # making GET request
 conn = http.client.HTTPSConnection("cdn-api.co-vin.in")
 payload = ''
 headers = {
   'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'
 }
-query = "/api/v2/appointment/sessions/public/findByDistrict?district_id=505&date="+str(d1)
+query = f"/api/v2/appointment/sessions/public/findByDistrict?district_id={dist_id}&date={d1}"
 conn.request("GET", query, payload, headers)
 res = conn.getresponse()
 data = res.read()
@@ -26,13 +27,14 @@ for x in dataDICT:
     if(isinstance(dataDICT[x], list)):
         count += len(dataDICT[x])
 now = (datetime.now()).strftime("%H:%M:%S")
-ininfo1 = f"Vaccination Slot Info JPR1 - {d1} {now}"
+dist_name = dataDICT["sessions"][0]['district_name']
+ininfo1 = f"Vaccination Slot Info {dist_name} - {d1} {now}"
 info1 = ""
 # printing centres information
 for i in range(0,count):
     dose1 = dataDICT["sessions"][i]['available_capacity_dose1']
     min_age = int(dataDICT["sessions"][i]['min_age_limit'])
-    if min_age==18 & dose1>0:
+    if min_age==18:
         info1 += f"""
                 ************************
                 Center Name : {dataDICT["sessions"][i]['name']}
@@ -42,7 +44,6 @@ for i in range(0,count):
                 Available Capacity Dose 2 : {dataDICT["sessions"][i]['available_capacity_dose1']}
                 Fee : {dataDICT["sessions"][i]['fee_type']}
                 Pincode : {dataDICT["sessions"][i]['pincode']}
-                ************************"""
 
 # publish notification
 API_KEY = "YOUR_API_KEY"
